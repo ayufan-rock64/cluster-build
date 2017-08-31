57c4668d0cfdc77886c073dbadd9a0bc08cde415
Kamil Trzcinski <ayufan@ayufan.eu>
2017-08-31 17:10:33 +0200

    Allow to compile pine64_plus and sopine immediately

--- Makefile.sunxi.mk
+++ Makefile.sunxi.mk
@@ -12,16 +12,28 @@ arm-trusted-firmware-sunxi/build/sun50iw1p1/debug/bl31.bin: arm-trusted-firmware
 u-boot-sunxi:
 	git clone https://github.com/linux-sunxi/u-boot-sunxi $@ --single-branch --depth=30
 
-u-boot-sunxi/configs/$(UBOOT_CONFIG)_defconfig: u-boot-sunxi
+u-boot-sunxi/configs/%_defconfig: u-boot-sunxi
 
-u-boot-sunxi/.config: u-boot-sunxi/configs/$(UBOOT_CONFIG)_defconfig
-	make -C u-boot-sunxi $(UBOOT_CONFIG)_defconfig
+tmp/u-boot-sunxi-%/.config: u-boot-sunxi/configs/%_defconfig
+	make -C u-boot-sunxi $(shell basename $<) KBUILD_OUTPUT=$(CURDIR)/$(shell dirname $@)
 
-u-boot-sunxi/spl/sunxi-spl.bin u-boot-sunxi/u-boot.itb: u-boot-sunxi/.config arm-trusted-firmware-sunxi/build/sun50iw1p1/debug/bl31.bin
-	make -C u-boot-sunxi ARCH=arm CROSS_COMPILE="$(LINARO_CC)" BL31="$(CURDIR)/$(word 2,$^)" u-boot-sunxi-with-spl.bin spl/sunxi-spl.bin u-boot.itb -j4
+tmp/u-boot-sunxi-%/spl/sunxi-spl.bin tmp/u-boot-sunxi-%/u-boot.itb: tmp/u-boot-sunxi-%/.config arm-trusted-firmware-sunxi/build/sun50iw1p1/debug/bl31.bin
+	make -C u-boot-sunxi ARCH=arm CROSS_COMPILE="$(LINARO_CC)" BL31="$(CURDIR)/$(word 2,$^)" u-boot-sunxi-with-spl.bin spl/sunxi-spl.bin u-boot.itb -j4 KBUILD_OUTPUT=$(CURDIR)/$(shell dirname $<)
 
-image/$(UBOOT_CONFIG)-uboot.bin: u-boot-sunxi/spl/sunxi-spl.bin u-boot-sunxi/u-boot.itb
+tmp/u-boot-sunxi-%/u-boot-with-spl.bin: tmp/u-boot-sunxi-%/spl/sunxi-spl.bin tmp/u-boot-sunxi-%/u-boot.itb
 	cat $^ > $@.tmp
 	mv $@.tmp $@
 
-image-sunxi: image/$(UBOOT_CONFIG)-uboot.bin
+image/pine64_plus-uboot.bin: tmp/u-boot-sunxi-pine64_plus/u-boot-with-spl.bin
+	cp $^ $@
+
+image/pine64_plus-uboot.bin: tmp/u-boot-sunxi-pine64_plus/u-boot-with-spl.bin
+	cp $^ $@
+
+image/sopine_baseboard-uboot.bin: tmp/u-boot-sunxi-sopine_baseboard/u-boot-with-spl.bin
+	cp $^ $@
+
+image-sunxi: \
+	image/pine64_plus-uboot.bin \
+	image/sopine_baseboard-uboot.bin
+
