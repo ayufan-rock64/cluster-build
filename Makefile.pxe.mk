6d4d072609f0fa26fe12a9596da4deb34a6bda7f
Kamil Trzciński <ayufan@ayufan.eu>
2018-01-16 12:03:27 +0100

    Use separate pxelinux.cfg for different archs

--- Makefile.pxe.mk
+++ Makefile.pxe.mk
@@ -2,9 +2,5 @@ tftproot/pxelinux.cfg/%: blobs/pxelinux.cfg/%
 	mkdir -p $$(dirname $@)
 	cp $< $@
 
-tftproot/extlinux/extlinux.conf: blobs/pxelinux.cfg/default-arm-rockchip
-	mkdir -p tftproot/extlinux
-	cp $< $@
-
 tftproot-pxelinux.cfg: \
 	$(patsubst blobs/%, tftproot/%, $(wildcard blobs/pxelinux.cfg/*))
